a802c6491bacc1d78bb356e3343055c17bd0ca1e
Leonardo Espindula <leonardoespindula@hotmail.com>
2024-04-18 22:50:52 -0300

    Aula #4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <TheHeader></TheHeader>
+  <TheHeader
+    v-if="showHeader"
+  />
+
+  <div v-show="ShowName">
+    Nome: {{  firstName  }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'" >Admin</div>
+  <div v-else-if="accessLevel === 'marketing'" >Marketing</div>
+  <div v-else >User</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -13,6 +25,15 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  data(){
+    return{
+      showHeader: true,
+      firstName: 'Jhon',
+      lastName: 'Snow',
+      ShowName: false,
+      accessLevel: 'admin'
+    }
   }
 }
 </script>
